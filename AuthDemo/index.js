b45a53faf14720d18bdaf6cce1467fb6e257279b
irensula <irinawebdev@gmail.com>
2024-02-23 22:11:55 +0200

    Add files via upload

--- AuthDemo/index.js
+++ AuthDemo/index.js
@@ -40,11 +40,7 @@ app.get('/register', (req, res) => {
 
 app.post('/register', async(req, res) => {
     const { password, username } = req.body;
-    const hash = await bcrypt.hash(password, 12);
-    const user = new User({
-        username,
-        password: hash
-    })
+    const user = new User({ username, password });
     await user.save();
     req.session.user_id = user._id;
     res.redirect('/');
@@ -56,13 +52,14 @@ app.get('/login', (req, res) => {
 
 app.post('/login', async (req, res) => {
     const { username, password } = req.body;
+    const foundUser = await User.findAndValidate(username, password);
     const user = await User.findOne({ username });
     const validPassword = await bcrypt.compare(password, user.password);
-    if(validPassword) {
-        req.session.user_id = user._id;
-        res.redirect('/secret')
+    if(foundUser) {
+        req.session.user_id = foundUser._id;
+        res.redirect('/secret');
     } else {
-        res.send('Try again.');
+        res.send('/login');
     }
 })
 
